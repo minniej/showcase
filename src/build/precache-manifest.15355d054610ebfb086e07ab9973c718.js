9aed4a6746d20fe65cbbb9a0632633bf88aac744
MINNIE <minniejung11@gmail.com>
2020-08-17 13:21:09 +0900

    first commit

---
+++ src/build/precache-manifest.15355d054610ebfb086e07ab9973c718.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c38546edbddd12eda77c3bcac263009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76e023682137400b13ea",
+    "url": "/static/css/main.180a81cc.chunk.css"
+  },
+  {
+    "revision": "9f27527568c48733aa8c",
+    "url": "/static/js/2.ca0bc356.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca0bc356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76e023682137400b13ea",
+    "url": "/static/js/main.6868fb6b.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
